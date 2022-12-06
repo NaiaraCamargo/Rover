009f17226a579c5d63e1132890e00d94dfa34cd4
NaiaraCamargo <naiaracamargo@outlook.com.br>
2022-12-06 08:56:55 -0300

    Changes to be committed: 	modified:   public/rover.html 	modified:   public/rover.js 	modified:   public/streaming.html 	modified:   public/streaming.js

--- public/streaming.js
+++ public/streaming.js
@@ -3,14 +3,7 @@ const socket = io();
 (function () {    
 
   const video = document.querySelector("#video");
-  const btnPlay = document.querySelector("#btnPlay");
-  const btnPause = document.querySelector("#btnPause");
-  const btnScreenshot = document.querySelector("#btnScreenshot");
-  const btnChangeCamera = document.querySelector("#btnChangeCamera");
-  const screenshotsContainer = document.querySelector("#screenshots");
-  const canvas = document.querySelector("#canvas");
-  const devicesSelect = document.querySelector("#devicesSelect");
- 
+
   // video constraints
   const constraints = {
     video: {
@@ -33,51 +26,11 @@ const socket = io();
   // current video stream
   let videoStream;
  
-  // handle events
-  // play
-  btnPlay.addEventListener("click", function () {
-    video.play();
-    btnPlay.classList.add("is-hidden");
-    btnPause.classList.remove("is-hidden");
-  });
  
-  // pause
-  btnPause.addEventListener("click", function () {
-    video.pause();
-    btnPause.classList.add("is-hidden");
-    btnPlay.classList.remove("is-hidden");
-  });
- 
-  // take screenshot
-  btnScreenshot.addEventListener("click", function () {
-    const img = document.createElement("img");
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    canvas.getContext("2d").drawImage(video, 0, 0);
-    img.src = canvas.toDataURL("image/png");
-    screenshotsContainer.prepend(img);
-  });
- 
-  // switch camera
-  btnChangeCamera.addEventListener("click", function () {
-    useFrontCamera = !useFrontCamera;
- 
-    initializeCamera();
-  });
- 
-  // stop video stream
-  function stopVideoStream() {
-    if (videoStream) {
-      videoStream.getTracks().forEach((track) => {
-        track.stop();
-      });
-    }
-  }
+
  
   // initialize
   async function initializeCamera() {
-    stopVideoStream();
-    constraints.video.facingMode = useFrontCamera ? "user" : "environment";
  
     try {
       videoStream = await navigator.mediaDevices.getUserMedia(constraints);
